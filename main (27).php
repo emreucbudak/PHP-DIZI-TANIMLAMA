976dc96cd082a7d8d6f004d9ffc8194d9e5ffa86
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 23:01:19 +0300

    Add files via upload

---
+++ main (27).php
@@ -0,0 +1,22 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniversitesi
+
+*******************************************************************************/
+// DİZİ TANIMLAMA
+$sayi = array("1","2","3");
+echo($sayi[0]);
+// AYRIYETEN BOŞ DİZİ TANIMLANIP ICINE ELEMAN EKLENEBILIR
+$sayi1 = [];
+$sayi1[0] = "Emre";
+echo($sayi1[0]);
+// DİZİDE BULUNAN DEĞERLERİN İNDİS DEĞERLERİYLE BİRLİKTE SIRALANMASINI ISTIYORSANIZ print_r();
+
+
+
+
+
+
+
+?>
